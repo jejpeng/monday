8affbbecd1852b7eeec33ad8a8b96981c656c21f
Jenny Peng <jejpeng@Jennys-MacBook-Pro.local>
2025-06-11 14:14:38 -0700

    params

--- monday.py
+++ monday.py
@@ -7,6 +7,7 @@ import datetime
 from datetime import date
 import numpy as np
 from smtplib import SMTP
+import argparse
 
 ####### CHANGE THESE VARIABLES AS NEEDED ##########
 maximum = 330 # maximum number of members on your Monday.com plan
@@ -20,9 +21,7 @@ numUsersToRemove = 5
 # Go to Monday.com -> click on your profile picture in the right corner -> Administration
 # -> Connections -> Personal API token. 
 # Generate the token and copy it.
-# Create the config.env inside the same folder that this file, monday.py, is in. 
-# Then, inside config.env, write API_KEY = <replace this text with the personal API token that you just copied, without the brackets>
-# save it and you're good to go!
+# Paste it here:
 # NEVER PUBLICLY SHOW YOUR API KEY. DO NOT PUBLISH IT ANYWHERE ONLINE. 
 # PUT IT IN A SAFE DOCUMENT SOMEWHERE AND NOWHERE ELSE.
 
@@ -31,10 +30,16 @@ numUsersToRemove = 5
 load_dotenv("config.env")
 API_KEY = os.getenv("API_KEY")
 
+
 timeLimit = timeLimitMonths * 30 * 24 * 60 * 60 # time in seconds # 63072000 
 mondayApiUrl = "https://api.monday.com/v2"
-headers = { "Authorization": API_KEY }
+# from https://stackoverflow.com/questions/7427101/simple-argparse-example-wanted-1-argument-3-results
+parser = argparse.ArgumentParser()
+parser.add_argument("a", nargs='?', default="empty_string")
+args = parser.parse_args()
 
+# arguments for the request
+headers = { "Authorization": API_KEY }
 queryNumUsers = '{ users { name email is_view_only last_activity id } }'
 data  = { 'query': queryNumUsers }
 
@@ -54,13 +59,24 @@ resultsDF = resultsDF.sort_values(by='time_since_last_activity', ascending=False
 inactiveUsers = resultsDF[(resultsDF['time_since_last_activity'] >= timeLimit) & (resultsDF['is_view_only'] == False)]['id'].astype('int').tolist()
 # this is the number of users with editing permissions that haven't been active in 'timeLimit' amount of time
 
-if len(inactiveUsers) > 0:
-    queryChangePermissions = 'mutation { update_users_role ( user_ids: ' + str(inactiveUsers) + ', new_role: VIEW_ONLY ) { updated_users { name id is_view_only } errors { user_id code message } } }'
-    data2 = { 'query': queryChangePermissions }
-    req2 = requests.post(url = mondayApiUrl, json = data2, headers = headers)
-    queryChangeResults = req2.json()
-    #print(queryChangeResults)
+if args.a == 'empty_string':
+    print("empty arguments")
+    print(f"Number of admin/members: {numUsers}")
+    print(f"Number of inactive admin/members: {inactiveUsers}")
 
+elif (args.a == 'remove') or (args.a == 'Remove'):
+    if len(inactiveUsers) > 0:
+        queryChangePermissions = 'mutation { update_users_role ( user_ids: ' + str(inactiveUsers) + ', new_role: VIEW_ONLY ) { updated_users { name id is_view_only } errors { user_id code message } } }'
+        data2 = { 'query': queryChangePermissions }
+        req2 = requests.post(url = mondayApiUrl, json = data2, headers = headers)
+        queryChangeResults = req2.json()
+        #print(queryChangeResults)
+    else:
+        print("No inactive users to remove.")
+else:
+    print("Unclear.")
+
+'''
 if numUsers >= maximum:
     print("") # ideas: - if over by x, change least active x users to view only
     # send email? see if the python script works in power automate 
@@ -75,3 +91,4 @@ else:
 
 print(numUsers)
 #print(today)
+'''
